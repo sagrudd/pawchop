a52bf953c2ba5262836ee794d38c4de9e4681119
sagrudd <stephen.rudd@nanoporetech.com>
2023-01-13 18:25:42 +0100

    directory traversal

--- src/adapter_scan.rs
+++ src/adapter_scan.rs
@@ -17,8 +17,32 @@ impl ObservedAdapters {
 
 
 
-pub fn input_scan(filepath: &String, adapterc: &AdapterCatalog, observed: &ObservedAdapters) {
-    
+pub fn input_scan(filepath: &Path, adapterc: &AdapterCatalog, observed: &ObservedAdapters) {
+    println!("parsing file [{filepath:?}]");
+}
+
+pub fn scan_directory(filepath: &Path, adapterc: &AdapterCatalog, observed: &ObservedAdapters, depth: u8) {
+    for entry in filepath.read_dir().expect("read_dir call failed") {
+        if let Ok(entry) = entry {
+            let pstr = entry.path();
+            let pp = pstr.as_path();
+            println!("{:?}", pp);
+            if pp.is_dir() && depth == 0 {
+                println!("{:?} is a directory", pp);
+                scan_directory(pp, adapterc, &observed, depth+1);
+            } else {
+                input_scan(pp, adapterc, &observed);
+            }
+        }
+
+
+    }
+    /* 
+    if 1 > 0 {
+        scan_directory(filepath, adapterc, observed, depth+1);
+
+    }
+    */
 }
 
 
@@ -34,15 +58,16 @@ pub fn scan_for_adapter_sequences(pawchop: &cmdline::PawchopCmd, adapterc: &Adap
         if pp.exists() {
             if pp.is_dir() {
                 println!("{} is a directory", &file_str);
+                scan_directory(pp, adapterc, &observed_adapters, 0);
             } else {
-
+                input_scan(pp, adapterc, &observed_adapters);
             }
         } else {
             return Err(String::from(format!("specified file path [{}] does not exist", &file_str)));
         }
         //input_scan(file_input.unwrap(), adapterc, &observed_adapters);
     } else {
-        return Err(String::from("This is FUBAR"));
+        return Err(String::from("This is an unlikely exception - filepath not specified"));
     }
 
     return Ok(observed_adapters);
